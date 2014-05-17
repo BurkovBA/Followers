7ed19ca46920ff7767483d5b2e6f5661b6075b26
Boris Burkov <whois burkov.net>
2014-05-17 13:26:46 +0400

    Basic implementation of editable list of followers in admin page.

--- followers/admin.py
+++ followers/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 
 from followers.models import ManMan
 
-admin.site.register(ManMan)
+class ManAdmin(admin.ModelAdmin):
+    list_display = ('name', 'follows', 'followed_by')
+    list_display_links = ('name',)
 
+    def follows(self, obj):
+        return len(obj.follow_ids.split(" "))
+
+    def followed_by(self, obj):
+        counter = 0
+        for man in ManMan.objects.all():
+            followed = str(man.follow_ids).split(" ")
+            for celebrity in followed:
+                if celebrity and int(celebrity) == int(obj.id): counter += 1
+        return str(counter)
+
+admin.site.register(ManMan, ManAdmin)
