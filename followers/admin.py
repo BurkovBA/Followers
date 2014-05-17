255e6071658d68de716074d765eda99edbf32bb0
Boris Burkov <whois burkov.net>
2014-05-17 21:23:10 +0400

    Add/change page layed out appropriately, but multiselection population failed.

--- followers/admin.py
+++ followers/admin.py
@@ -1,21 +1,80 @@
 from django.contrib import admin
-# Register your models here.
+from django import forms
 
 from followers.models import ManMan
 
+class ManForm(forms.ModelForm):
+    follows_choices = () # will be dynamically set from ManAdmin.get_form
+    followed_by_choices = () # will be dynamically set from ManAdmin.get_form
+    follows = forms.MultipleChoiceField(label='follows', choices=follows_choices)
+    followed_by = forms.MultipleChoiceField(label='followed by', choices=followed_by_choices)
+
+    class Meta:
+        model = ManMan
+        fields = ['name', 'follows', 'followed_by']
+
 class ManAdmin(admin.ModelAdmin):
-    list_display = ('name', 'follows', 'followed_by')
+    list_display = ('name', 'len_follows', 'len_followed_by')
     list_display_links = ('name',)
+    form = ManForm
+
+    def save_form(self, request, obj=None, **kwargs):
+        '''
+        This is an ugly hack, created for the following purpose:
+
+        I want to customize the add/change page, by making it display 
+        Multiselections with followers of currently selected Man. But
+        I can't reach the currently selected Man from ManForm! 
+
+        To overcome that, I recover Man's id from the request argument
+        of this function and dynamically modify follows_choices and 
+        followed_by_choices attributes of ManForm class. Yuck.
+
+        TODO: Thread-safety!
+        TODO: Exception handling!
+        '''
+        # get last component of path, it is Man id
+        components = request.path.split("/")
+        if len(components) > 1 and components[-1] == "":
+            id = int(components[-2])
+        elif len(components) > 0: 
+            id = int(components[-1])
+        man = ManMan.objects.get(id=id)
+
+        # prepare 2-tuples in ManForm
+        followers = self.follows(man)
+        follows_choices = []
+        for follower in followers:
+            follows_choices.append((follower, follower))
+        ManForm.follows_choices = tuple(follows_choices)
+
+        followed_by = self.followed_by(man)
+        followed_by_choices = []
+        for followed in followed_by:
+            followed_by_choices.append((followed, followed))
+        ManForm.followed_by_choices = tuple(followed_by_choices)
+
+        return super(ManAdmin, self).save_form(request, obj, **kwargs)
 
     def follows(self, obj):
-        return len(obj.follow_ids.split(" "))
+        '''returns list of Men, whom obj follows'''
+        return obj.follow_ids.split(" ")
+
+    def len_follows(self, obj):
+        return str(len(self.follows(obj)))
 
     def followed_by(self, obj):
-        counter = 0
-        for man in ManMan.objects.all():
-            followed = str(man.follow_ids).split(" ")
-            for celebrity in followed:
-                if celebrity and int(celebrity) == int(obj.id): counter += 1
-        return str(counter)
+        '''returns list of Men, who follow obj'''
+        output = []
+        for follower in ManMan.objects.all():
+            all_followed = str(follower.follow_ids).split(" ")
+            for followed in all_followed:
+                if followed and int(followed) == int(obj.id): 
+                    output.append(unicode(follower.id))
+                    break
+        return output
+
+    def len_followed_by(self, obj):
+        return str(len(self.followed_by(obj)))
 
 admin.site.register(ManMan, ManAdmin)
